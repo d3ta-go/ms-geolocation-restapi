f0c6ecba90fce9a903c93e527a3ad4c2ed26b589
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-20 09:35:18 +0700

    add build binary distribution script

---
+++ build.dist.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# bash
+BIN_FILE=ms-geolocation-restapi
+
+platforms=("darwin/amd64" "linux/amd64" "linux/386" "windows/amd64" "windows/386")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$BIN_FILE
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    echo "build \"$output_name\" for $GOOS/$GOARCH ..."
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+
+    OS_DIR=$GOOS'-'$GOARCH
+    if [ -f "$output_name" ]; then
+        if [ -d "dist/$OS_DIR/" ]; then
+            rm -rf dist/$OS_DIR/
+            sleep 2
+        fi
+
+        mkdir -p dist/$OS_DIR/conf/casbin
+        mkdir -p dist/$OS_DIR/conf/data
+
+        mv $output_name dist/$OS_DIR/
+
+        cp conf/config.yaml dist/$OS_DIR/conf/
+        cp conf/casbin/casbin_rbac_rest_model.conf dist/$OS_DIR/conf/casbin/
+        cp conf/data/test-data.yaml dist/$OS_DIR/conf/data/
+        
+        cp -R www dist/$OS_DIR/
+    fi
+done
